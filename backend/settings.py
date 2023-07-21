e2f5024474ff19814feae62c0a6c1c4721c26c7f
arcenioCorteis <arcenioCorteis@github.com>
2023-07-21 19:54:58 +0200

    feat: add react port

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'todo',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+# react Port 
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000'
+)
 
 
 # Internationalization
