b421ef0e17493cc9f8b07879d7f872f7b29ec8d1
arcenioCorteis <arcenioCorteis@github.com>
2023-07-23 15:13:37 +0200

    feat: List TAsk

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Todo(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=500)
+    title = models.CharField(max_length=50, null=True)
+    description = models.CharField(max_length=500, null=True)
     completed = models.BooleanField(default=False)
     
     
